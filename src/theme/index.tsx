2d94ea59afdeee6148b59755f7c93f719150b867
Nam Nguyen <nam.nguyen@kyber.network>
2022-04-27 18:32:36 +0700

    fix safari black text
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -143,7 +143,7 @@ export default function ThemeProvider({ children }: { children: React.ReactNode 
 }
 
 const TextWrapper = styled(Text)<{ color: keyof Colors }>`
-  color: ${({ color, theme }) => (theme as any)[color]};
+  color: ${({ color, theme }) => (theme as any)[color]} !important;
 `
 
 export const TYPE = {
