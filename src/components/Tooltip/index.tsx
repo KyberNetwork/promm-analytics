8851c307d8a8f7356685d57a089188f0836e148c
Nam Nguyen <nam.nguyen@kyber.network>
2022-06-07 13:55:02 +0700

    fix links & pool chart (#14)
    * fix links
    
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>
    
    * improve pool chart
    
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import Popover, { PopoverProps } from '../Popover'
 
 const TooltipContainer = styled.div<{ fontSize?: number }>`
-  width: 228px;
+  max-width: 228px;
   padding: 0.6rem 1rem;
   line-height: 150%;
   font-weight: 400;
