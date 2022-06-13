4bec24ff8e0fc27b8cb2d67407c33999c142f522
Nam Nguyen <nam.nguyen@kyber.network>
2022-06-13 15:35:17 +0700

    proamm -> elastic
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -124,7 +124,7 @@ type PoolLinkInfo =
     }
 
 export function getPoolLink(info: PoolLinkInfo, networkInfo: NetworkInfo): string {
-  const swapURL = (process.env.REACT_APP_DMM_SWAP_URL || 'https://kyberswap.com') + '/proamm'
+  const swapURL = (process.env.REACT_APP_DMM_SWAP_URL || 'https://kyberswap.com') + '/elastic'
   let resultURL
 
   if (info.type === 'remove') {
