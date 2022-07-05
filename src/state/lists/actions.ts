c1e885023aa1bfcbcddafb330e61f776feda89ac
Nam Nguyen <nam.nguyen@kyber.network>
2022-07-05 11:00:10 +0700

    remove tokenlist check (#33)
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>

--- src/state/lists/actions.ts
+++ src/state/lists/actions.ts
@@ -1,5 +1,5 @@
 import { ActionCreatorWithPayload, createAction } from '@reduxjs/toolkit'
-import { TokenList, Version } from '@uniswap/token-lists'
+import { TokenList } from '@uniswap/token-lists'
 
 export const fetchTokenList: Readonly<{
   pending: ActionCreatorWithPayload<{ url: string; requestId: string }>
@@ -10,14 +10,3 @@ export const fetchTokenList: Readonly<{
   fulfilled: createAction('lists/fetchTokenList/fulfilled'),
   rejected: createAction('lists/fetchTokenList/rejected'),
 }
-// add and remove from list options
-export const addList = createAction<string>('lists/addList')
-export const removeList = createAction<string>('lists/removeList')
-
-// // select which lists to search across from loaded lists
-// export const enableList = createAction<string>('lists/enableList')
-// export const disableList = createAction<string>('lists/disableList')
-
-// versioning
-export const acceptListUpdate = createAction<string>('lists/acceptListUpdate')
-export const rejectVersionUpdate = createAction<Version>('lists/rejectVersionUpdate')
